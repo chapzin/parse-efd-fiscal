80c0aa943a7ba33a1b6ca3e853731e937e6f8b3a
Holanda Junior <joaoholandajr@gmail.com>
2017-03-07 09:33:48 -0300

    Refatorando codigo

--- model/Bloco0/Reg0000.go
+++ model/Bloco0/Reg0000.go
@@ -35,100 +35,32 @@ type Reg0000Sped struct {
 	Ln []string
 }
 
-func (s Reg0000Sped) GetReg() string{
-	return s.Ln[1]
-}
-
-func (s Reg0000Sped) GetCodVer() string {
-	return s.Ln[2]
-}
-
-func (s Reg0000Sped) GetCodFin() int {
-	return SpedConvert.ConvInt(s.Ln[3])
-}
-
-func (s Reg0000Sped) GetDtIni() time.Time {
-	return SpedConvert.ConvertData(s.Ln[4])
-}
-
-func (s Reg0000Sped) GetDtFin() time.Time {
-	return SpedConvert.ConvertData(s.Ln[5])
-}
-
-func (s Reg0000Sped) GetNome() string {
-	return s.Ln[6]
-}
-
-func (s Reg0000Sped) GetCnpj() string {
-	return  s.Ln[7]
-}
-
-func (s Reg0000Sped) GetCpf() string {
-	return s.Ln[8]
-}
-
-func (s Reg0000Sped) GetUf() string  {
-	return s.Ln[9]
-}
-
-func (s Reg0000Sped) GetIe() string  {
-	return s.Ln[10]
-}
-
-func (s Reg0000Sped) GetCodMun() string  {
-	return s.Ln[11]
-}
-
-func (s Reg0000Sped) GetIm() string {
-	return s.Ln[12]
-}
-
-func (s Reg0000Sped) GetSuframa() string  {
-	return s.Ln[13]
-}
-
-func (s Reg0000Sped) GetIndPerfil() string  {
-	return s.Ln[14]
-}
-
-func (s Reg0000Sped) GetIndAtiv() int  {
-	return SpedConvert.ConvInt(s.Ln[15])
-}
-
 func CreateReg0000 (read iReg0000) Reg0000 {
-	reg0000 := Reg0000{
-		Reg:		read.GetReg(),
-		CodVer:		read.GetCodVer(),
-		CodFin:		read.GetCodFin(),
-		DtIni:		read.GetDtIni(),
-		DtFin:		read.GetDtFin(),
-		Nome:		read.GetNome(),
-		Cnpj:		read.GetCnpj(),
-		Cpf:		read.GetCpf(),
-		Uf:		read.GetUf(),
-		Ie:		read.GetIe(),
-		CodMun:		read.GetCodMun(),
-		Im:		read.GetIm(),
-		Suframa:	read.GetSuframa(),
-		IndPerfil:	read.GetIndPerfil(),
-	}
-	return reg0000
+	return read.GetReg0000()
 }
 
 type iReg0000 interface {
-	GetReg() string
-	GetCodVer() string
-	GetCodFin() int
-	GetDtIni() time.Time
-	GetDtFin() time.Time
-	GetNome() string
-	GetCnpj() string
-	GetCpf() string
-	GetUf() string
-	GetIe() string
-	GetCodMun() string
-	GetIm() string
-	GetSuframa() string
-	GetIndPerfil() string
-	GetIndAtiv() int
+	GetReg0000() Reg0000
+}
+
+func (s Reg0000Sped) GetReg0000() Reg0000 {
+	reg0000 := Reg0000{
+			Reg:		s.Ln[1],
+			CodVer:		s.Ln[2],
+			CodFin:		SpedConvert.ConvInt(s.Ln[3]),
+			DtIni:		SpedConvert.ConvertData(s.Ln[4]),
+			DtFin:		SpedConvert.ConvertData(s.Ln[5]),
+			Nome:		s.Ln[6],
+			Cnpj:		s.Ln[7],
+			Cpf:		s.Ln[8],
+			Uf:			s.Ln[9],
+			Ie:			s.Ln[10],
+			CodMun:		s.Ln[11],
+			Im:			s.Ln[12],
+			Suframa:	s.Ln[13],
+			IndPerfil:	s.Ln[14],
+			IndAtiv:	SpedConvert.ConvInt(s.Ln[15]),
+		}
+
+	return reg0000
 }
