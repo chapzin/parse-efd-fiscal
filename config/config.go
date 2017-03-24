fbbdd2ecaa9aa6bfd257074cb79e8943bf1b1202
Holanda Junior <joaoholandajr@gmail.com>
2017-03-23 23:36:28 -0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -12,7 +12,7 @@ var (
 )
 
 type ConfigInterface interface {
-	ObterTexto(chave string) (string, error)
+	ObterTexto( chave string ) ( string, error )
 	GetConfiguracoes()
 }
 
@@ -21,11 +21,12 @@ type Configurador struct {
 }
 
 //ObterTexto é um método responsavel por obter valor de texto do arquivo de propriedades do sistema
-func (cfg Configurador) ObterTexto(chave string) (string, error) {
-	valor, err := c.String("DEFAULT", chave)
+func ( cfg Configurador ) ObterTexto( chave string ) ( string, error ) {
+	
+	valor, err := c.String( "DEFAULT", chave )
 	if err != nil {
-		fmt.Println("Falha ao obter o valor de texto", chave)
 
+		fmt.Println( "Falha ao obter o valor de texto", chave )
 		return "", err
 	}
 
@@ -34,16 +35,16 @@ func (cfg Configurador) ObterTexto(chave string) (string, error) {
 
 func (cfg Configurador) GetConfiguracoes() {
 
-	config, err := conf.ReadDefault("config/config.cfg")
+	config, err := conf.ReadDefault( "config/config.cfg" )
 	if err != nil {
-		panic("Arquivo não encontrado")
+		panic( "Arquivo não encontrado" )
 	}
 
 	c = config
 
 }
 
-func InicializaConfiguracoes(prop ConfigInterface) {
+func InicializaConfiguracoes( prop ConfigInterface ) {
 	Propriedades = prop
 	Propriedades.GetConfiguracoes()
 
